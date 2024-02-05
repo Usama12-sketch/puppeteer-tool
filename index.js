246c2ad2b2e043310379e7257437934765b9f3db
usama12-sketch <ilmohikmat11@gmail.com>
2024-02-05 01:02:01 -0800

    retry with output excel data

--- index.js
+++ index.js
@@ -44,16 +44,74 @@ for (let rowNum = range.s.r; rowNum <= range.e.r; rowNum++) {
 console.log('Column G Data:', columnGData);
 console.log('Column I Data:', columnKData);
 
+
+
+const workbook2 = xlsx.readFile('output.xlsx');
+const sheetName2 = workbook2.SheetNames[0]; // Assuming you have only one sheet
+const worksheet2 = workbook2.Sheets[sheetName2];
+
+const range2 = xlsx.utils.decode_range(worksheet2['!ref']);
+range2.s.r = 1; // Start from row 2
+// const data = xlsx.utils.sheet_to_json(worksheet2, { header: 1 , range: 1});
+// console.log("main data hoon" , data)
+
+const columnAData = [];
+const columnBData = [];
+const columnCData = [];
+const columnDData = [];
+
+for (let rowNum = range2.s.r; rowNum <= range2.e.r; rowNum++) {
+  const cellAddressA = xlsx.utils.encode_cell({ r: rowNum, c: 0 }); // 6  to column G
+  const cellValueA = worksheet2[cellAddressA] ? worksheet2[cellAddressA].v : null;
+  
+ 
+  const cellAddressB = xlsx.utils.encode_cell({ r: rowNum, c: 1 }); // 6  to column G
+  const cellValueB = worksheet2[cellAddressB] ? worksheet2[cellAddressB].v : null;
+ 
+ 
+  const cellAddressC = xlsx.utils.encode_cell({ r: rowNum, c: 2 }); // 6  to column G
+  const cellValueC = worksheet2[cellAddressC] ? worksheet2[cellAddressC].v : null;
+  
+  const cellAddressD = xlsx.utils.encode_cell({ r: rowNum, c: 3 }); // 6  to column G
+  const cellValueD = worksheet2[cellAddressD] ? worksheet2[cellAddressD].v : null;
+
+    columnAData.push(cellValueA);
+    columnBData.push(cellValueB);
+    columnCData.push(cellValueC);
+    columnDData.push(cellValueD);
+  
+
+}
+
+// console.log('Column D Data:', columnDData , columnDData.length);
+
 async function run() {
   try {
     console.log("lenth of urls :" + columnGData.length)
     const data = [
       // ... Add your extracted data here
     ];
+       
+
+    for (let i = 0; i < columnCData.length; i++) {
+      const dataObject = {
+        text: columnAData[i],
+        price: columnBData[i],
+        URL: columnCData[i],
+         oldprice: columnDData[i]
+      };
+      data.push(dataObject);
+    }
+
+
+ console.log("data hon bhai dekh lo" , data)
     let dataObject
     
     let urlCount = 0
-    for (const url of columnGData) {
+    for (i= columnDData.length; i < columnGData.length; i++) {
+
+      let url = columnGData[i]
+      console.log("url runnning ... " , url)
       urlCount++;
       const browser = await puppeteer.launch({
         headless: "new",
@@ -64,7 +122,13 @@ async function run() {
       await page.setDefaultNavigationTimeout(100*60 * 1000)
       const randomNumber = Math.floor(Math.random() * 5) + 3;
       await page.waitForTimeout(2500 * randomNumber)
-      await page.goto(url);
+      if (url.includes("https")) {
+        await page.goto(url);
+        
+      } else {
+        
+        await page.goto(`https://${url}`);
+      }
       // const navigationPromise = await page.waitForNavigation({waitUntil: "domcontentloaded"});
       
       
@@ -79,7 +143,6 @@ async function run() {
         window.scrollTo(600, 700, {behavior: 'smooth'}); // Scroll to an offset of 500 pixels from the top with smooth animation
       });
       
-      await page.waitForSelector(".css-111drvy"  , {timeout: 24000})
       
       
       const Element = await page.$(".css-1nvyvo0");
@@ -90,7 +153,26 @@ async function run() {
 
       const htmlContent = await page.evaluate(() => document.getElementsByTagName("section")[2].innerHTML);
 
-      if (htmlContent){
+           if (Element) {
+          await page.screenshot({path: `Saboot/ss${url.length * Math.random()}.png`})
+          const ariaLabel = await Element.evaluate(el => el.textContent);
+    
+          
+            dataObject = { text: title, price: ariaLabel, URL: url , oldprice: columnKData[i]}
+            console.log(`URL: ${title}  element: ${ariaLabel}`);
+          }   
+          else if (chance2) {
+            await page.screenshot({path: `Saboot/ss${url.length * Math.random()}.png`})
+            const ariaLabel = await chance2.evaluate(el => el.textContent);
+            
+            
+            const title  = await page.title()
+            dataObject = { text: title, price: ariaLabel, URL: url,  oldprice: columnKData[i] }
+            
+            console.log(`URL: ${title}  chance2: ${ariaLabel}`);
+          }   
+          
+         else if (htmlContent){
 
         // Apply the code snippet to the HTML content
         const firstIndex = htmlContent.indexOf("$");
@@ -102,29 +184,11 @@ async function run() {
         }
     
         const extractedSubstring = htmlContent.substring(firstIndex, lastIndex);
-          dataObject = { text: title, price: extractedSubstring, url: url }
+          dataObject = { text: title, price: extractedSubstring, URL: url,  oldprice: columnKData[i] }
     
           console.log(`title: ${title}  han bhai mn hoon sab ka baap: ${extractedSubstring}`);
       }
-      else if (Element) {
-          await page.screenshot({path: `Saboot/ss${url.length * Math.random()}.png`})
-          const ariaLabel = await Element.evaluate(el => el.textContent);
-    
-          
-            dataObject = { text: title, price: ariaLabel, url: url }
-            console.log(`URL: ${title}  element: ${ariaLabel}`);
-          }   
-          else if (chance2) {
-            await page.screenshot({path: `Saboot/ss${url.length * Math.random()}.png`})
-            const ariaLabel = await chance2.evaluate(el => el.textContent);
-            
-            
-            const title  = await page.title()
-            dataObject = { text: title, price: ariaLabel, url: url }
-            
-            console.log(`URL: ${title}  chance2: ${ariaLabel}`);
-          }   
-          
+
          else  if(Value1){
             const Value1 = await page.$(".css-1qfcjyj");
           
@@ -136,7 +200,7 @@ async function run() {
             
             console.log(`URL: ${title}  seprated: ${firstText}.Error`);
             
-            dataObject = { text: title, price: firstText + ".Error" , url: url  }
+            dataObject = { text: title, price: firstText + ".Error" , URL: url,  oldprice: columnKData[i]  }
             
           }
           
@@ -147,7 +211,7 @@ async function run() {
           
           await page.screenshot({path: `Saboot/ss${url.length * Math.random()}.png`})
           
-          dataObject = { text: title, price: ".Error", url: url }
+          dataObject = { text: title, price: ".Error", URL: url ,  oldprice: columnKData[i]}
           console.log(`URL: ${title}, Element with class 'css-111drvy' not found`);
         }
 
@@ -172,9 +236,14 @@ async function run() {
           await new Promise(resolve => setTimeout(resolve, 2 * 60 * 1000)); // Delay for 2 minutes
         }
             
-        data.forEach((item , index) => {
-          item['Catch-price'] = columnKData[index];
-        });
+        // data.forEach((item , index) => {
+        //   item['Catch-price'] = columnKData[index];
+        // });
+
+          
+
+
+
         // Create a workbook and add a worksheet
         const workbook = xlsx.utils.book_new();
         const worksheet = xlsx.utils.json_to_sheet(data);
@@ -202,6 +271,7 @@ async function run() {
   } catch (err) {
     console.error("Error:", err.message);
     console.error(err.stack);
+    run()
   }
 }
 
